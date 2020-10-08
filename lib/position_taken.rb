458bd680964f894ca9da100fa59e79c75e19dd15
Hannah Malm <hannahemalm616@gmail.com>
2020-10-08 01:38:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -14,9 +14,9 @@
 
 def position_taken?(board,index)
   if board[index] == " " || board[index] == "" || board[index] == nil
-    "false"
+    false
   else board[index] == "X" || board[index] == "O"
-    "true"
+    true
   end
 end
   
